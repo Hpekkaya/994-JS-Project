91fbd40656c34d32f7ad2444a29c2185759a3627
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-19 19:45:25 +0300

    Random function is added

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Random number generate between (0-1000)
+
+console.log("This function generates number between (0-1000) \n \n")
+let num =Math.random()*1001 ;
+console.log("The random number => ", num.toFixed())
+
+
